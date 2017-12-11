47d94392826ef783a97d4a242feec0f6837cd577
Gaetan PUGET <gpuget@excilys.com>
2017-12-11 15:34:57 +0100

    [CONFIG] Sample test with Junit

---
+++ src/com/excilys/formation/xp/GeneralTest.java
@@ -0,0 +1,12 @@
+package com.excilys.formation.xp;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class GeneralTest extends TestCase {
+	@Test
+	public void test() {
+		// TODO
+	}
+}
