2d443b2006e5e3f79cb62cefdef8e080880dc464
Gaëtan <pugetgaetan@free.fr>
2018-12-14 13:24:58 +0100

    [REFACTO] Migrate on maven project and JUnit 5

---
+++ src/main/java/formation/xp/Main.java
@@ -0,0 +1,7 @@
+package formation.xp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("YOLOOOO");
+    }
+}
---
+++ src/test/java/formation/xp/MainTest.java
@@ -0,0 +1,13 @@
+package formation.xp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    @Test
+    public void test() {
+        // TODO
+        assertTrue(false);
+    }
+}
