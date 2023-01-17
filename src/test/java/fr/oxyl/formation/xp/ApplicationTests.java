c569c4f74ae5cb18d32f12226cb3fc6da6deb7da
gpuget <gpuget@excilys.com>
2023-01-17 18:27:09 +0100

    New project: Spring boot 2.7.7 and Java 8

---
+++ src/test/java/fr/oxyl/formation/xp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.oxyl.formation.xp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
