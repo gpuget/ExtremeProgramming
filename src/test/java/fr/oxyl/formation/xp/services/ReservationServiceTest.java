e1e5f59f036093dda3e5026155d88592f91de7c3
gpuget <gpuget@excilys.com>
2023-01-31 10:40:48 +0100

    Repository tests

--- src/test/java/fr/oxyl/formation/xp/services/ReservationServiceTest.java
+++ src/test/java/fr/oxyl/formation/xp/services/ReservationServiceTest.java
@@ -1,6 +1,13 @@
 package fr.oxyl.formation.xp.services;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import fr.oxyl.formation.xp.models.Reservation;
 import fr.oxyl.formation.xp.persistence.ReservationRepository;
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,6 +24,12 @@ class ReservationServiceTest {
 
   @Test
   void reserve() {
+    Reservation saved = new Reservation();
+    when(this.repository.save(any())).thenReturn(saved);
 
+    Reservation reservation = this.service.reserve();
+
+    verify(this.repository).save(any());
+    assertThat(reservation).isSameAs(saved);
   }
 }
