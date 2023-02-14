270f69ac8db599c4d65f3c4fac0bd8887f802fc1
gpuget <gpuget@excilys.com>
2023-02-14 09:08:55 +0100

    Reset project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,5 @@
-import './App.css';
-import {useEffect, useState} from "react";
-import {getReservations} from "./api/reservations";
-
 function App() {
-
-    const [reservations, setReservations] = useState();
-    const [tab,  setTab] = useState(false);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await getReservations();
-            setReservations(response);
-        }
-
-        fetchData();
-    }, []);
-
-    const toggle = () => {
-        setTab(tab => !tab);
-    };
-
-    return (
-        <div className="App">
-            <button onClick={toggle}>Change me</button>
-            { tab
-                ? (<div>
-                    {reservations && reservations
-                    .map(reservation => reservation.username)
-                    .map(username => (<p key={username}>{username}</p>))}
-                    </div>)
-                : (<div>
-                    {reservations && reservations
-                        .map(reservation => reservation.cinema)
-                        .map(cinema => (<p key={cinema}>{cinema}</p>))}
-                    </div>)}
-        </div>
-    );
+    return <div>Hello World</div>
 }
 
 export default App;
--- frontend/src/api/reservations.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const getReservations = () => {
-    return axios.get(`${process.env.REACT_APP_BACK_URL || ""}/reservations`)
-        .then(res => res.data);
-}
